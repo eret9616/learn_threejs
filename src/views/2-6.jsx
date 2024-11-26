04d4ce395647a31cebdd1c847a5c1b92577f033a
hh <947416983@qq.com>
2024-11-27 02:55:49 +0800

    018

---
+++ src/views/2-6.jsx
@@ -0,0 +1,170 @@
+import { useEffect } from 'react';
+import { render } from 'react-dom';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// DragControls
+import { DragControls } from 'three/examples/jsm/controls/DragControls.js';
+import * as dat from 'dat.gui';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+import { Wireframe } from 'three/examples/jsm/lines/Wireframe.js';
+import { HeartCurve } from 'three/examples/jsm/curves/CurveExtras.js';
+
+const Page = () => {
+  useEffect(() => {
+    const $ = {
+      createScene() {
+        const canvas = document.getElementById('c');
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        // Set the canvas width and height
+        canvas.height = height;
+        canvas.width = width;
+        this.canvas = canvas;
+        this.height = height;
+        this.width = width;
+        const scene = new THREE.Scene();
+        this.scene = scene;
+      },
+      createLights() {
+        // 添加全局光照
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+
+        this.scene.add(ambientLight, directionalLight);
+      },
+      createObjects() {
+        // 创建立方体几何体
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+        // 创建立方体的基础材质
+        const material = new THREE.MeshLambertMaterial({ color: 0x1890ff });
+        // 创建3D物体对象
+        const mesh = new THREE.Mesh(geometry, material);
+
+        this.scene.add(mesh);
+        this.mesh = mesh;
+      },
+      createCamera() {
+        const pCamera = new THREE.PerspectiveCamera(
+          75,
+          this.width / this.height,
+          0.1,
+          1000
+        );
+
+        pCamera.position.set(0, 1, 2); // x,y,z
+        pCamera.lookAt(this.scene.position);
+        this.scene.add(pCamera);
+        this.camera = pCamera;
+      },
+      datGui() {
+        const _this = this;
+        if (window.gui) {
+          window.gui.destroy();
+        }
+        const gui = new dat.GUI();
+        window.gui = gui;
+        gui.add(_this.orbitControls, 'enabled');
+        gui.add(_this.orbitControls, 'dampingFactor', 0.01, 0.2, 0.01); // 阻尼系数
+        gui.add(_this.orbitControls, 'enablePan'); // 旋转速度
+        // panspeed
+        gui.add(_this.orbitControls, 'panSpeed', 1, 10, 1);
+        gui.add(_this.orbitControls, 'autoRotate');
+        gui.add(_this.orbitControls, 'autoRotateSpeed', 1, 10, 1);
+        gui.add(_this.orbitControls, 'enableZoom');
+        gui.add(_this.orbitControls, 'zoomSpeed', 1, 10, 1);
+      },
+      helpers() {
+        const axesHelper = new THREE.AxesHelper();
+
+        this.scene.add(axesHelper);
+        // 创建辅助平面
+        const gridHelper = new THREE.GridHelper();
+        this.scene.add(gridHelper);
+      },
+      render() {
+        // 创建渲染器
+        const renderer = new THREE.WebGLRenderer({
+          canvas: this.canvas,
+          antialias: true,
+        });
+        // 设置渲染器屏幕像素比
+        renderer.setPixelRatio(window.devicePixelRatio || 1);
+
+        //设置渲染器大小
+        renderer.setSize(this.width, this.height);
+        // 执行渲染
+        renderer.render(this.scene, this.camera);
+        this.renderer = renderer;
+      },
+      controls() {
+        // 创建轨道控制器
+        const orbitControls = new OrbitControls(this.camera, this.canvas);
+        orbitControls.enableDamping = true;
+        this.orbitControls = orbitControls;
+        console.log(orbitControls);
+
+        const dragControls = new DragControls(
+          [this.mesh],
+          this.camera,
+          this.canvas
+        );
+
+        dragControls.addEventListener('dragstart', () => {
+          // 拖拽开启事件
+          orbitControls.enabled = false;
+        });
+
+        dragControls.addEventListener('dragend', () => {
+          // 拖拽结束事件
+          orbitControls.enabled = true;
+        });
+      },
+      count: 0, //当前点的index
+      tick() {
+        this.orbitControls.update();
+
+        this.renderer.render(this.scene, this.camera);
+        window.requestAnimationFrame(() => {
+          return this.tick();
+        });
+      },
+      fitView() {
+        window.addEventListener(
+          'resize',
+          () => {
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+            renderer.setSize(width, height);
+
+            camera.aspect = width / height;
+            camera.updateProjectionMatrix();
+          },
+          false //
+        );
+      },
+      init() {
+        this.createScene();
+        this.createLights();
+        this.createObjects();
+        this.createCamera();
+        this.helpers();
+        this.render();
+        this.controls();
+        this.tick();
+        this.fitView();
+        this.datGui();
+      },
+    };
+    $.init();
+  }, []);
+
+  return (
+    <>
+      start your project
+      <canvas id="c" />;
+    </>
+  );
+};
+
+export default Page;
